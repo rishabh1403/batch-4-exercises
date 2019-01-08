7ca06b067cfd4a55e56a323e0d579c189ac04d46
Arfat Salman <giney.paradise@gmail.com>
2019-01-08 20:26:28 +0530

    Exercises for day-6-segment-2

---
+++ sleep-promise/README.md
@@ -0,0 +1,10 @@
+// sleep-promise resolves a promise after a specified delay.
+// Usage
+// (async () => {
+//   await sleep(2000);
+//   console.log('2 seconds later …');
+// })();
+
+// sleep(2000).then(function() {
+//   console.log('2 seconds later …')
+// });
---
+++ sleep-promise/sleep.js
@@ -0,0 +1,24 @@
+function createSleepPromise(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+function sleep(timeout) {
+  const sleepPromise = createSleepPromise(timeout);
+
+  // Pass value through, if used in a promise chain
+  function promiseFunction(value) {
+    return sleepPromise.then(() => value);
+  }
+
+  // Normal promise
+  promiseFunction.then = (...args) => sleepPromise.then(...args);
+  promiseFunction.catch = Promise.resolve().catch;
+
+  return promiseFunction;
+}
+
+export {
+  sleep,
+};
