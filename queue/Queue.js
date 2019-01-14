e2fed81d96af0f9102e3061eded647c01d902f66
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-15 05:19:06 +0530

    complete question queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,35 @@
 
-function queue(...args) {
-  return args;
+class Queue {
+  constructor() {
+    this.linkedList = [];
+  }
+
+  enqueue(data) {
+    this.linkedList.push(data);
+  }
+
+  toString(stringifier) {
+    if (!stringifier) {
+      return this.linkedList.toString();
+    }
+    return this.linkedList.reduce((acc, el) => `${acc}${stringifier(el)},`, '').slice(0, -1);
+  }
+
+  peek() {
+    return this.linkedList[0] ? this.linkedList[0] : null;
+  }
+
+  isEmpty() {
+    return this.linkedList.length === 0;
+  }
+
+  dequeue() {
+    const [head, ...tail] = this.linkedList;
+    this.linkedList = tail;
+    return head === undefined ? null : head;
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
