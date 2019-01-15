5a7963f89244a764b0ad11ba0af3784c995b47ae
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-16 00:35:38 +0530

    complete question generator

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,10 @@
 
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable() {
+  let count = 1;
+  while (count < 6) {
+    yield count;
+    count += 1;
+  }
 }
 
 export {
