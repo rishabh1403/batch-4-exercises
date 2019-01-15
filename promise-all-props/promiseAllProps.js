a4c6e0ce3379bb3336796a5511bd0990535a426a
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-15 05:31:45 +0530

    complete question promise all props

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,19 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(props) {
+  return new Promise((resolve) => {
+    const keys = Object.keys(props);
+    const values = Object.values(props);
+    Promise.all(values)
+      .then((resolvedData) => {
+        resolve(resolvedData.reduce((acc, el, index) => {
+          const key = keys[index];
+          return {
+            ...acc,
+            [key]: el,
+          };
+        }, {}));
+      });
+  });
 }
 
 export {
