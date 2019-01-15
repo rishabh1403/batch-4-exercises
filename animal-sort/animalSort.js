5b0320b7ab9ce5d7955a497590a7bb73dff91181
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-16 03:53:51 +0530

    complete animal sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,18 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  animals.sort((a, b) => {
+    if (a.numberOfLegs !== b.numberOfLegs) {
+      return a.numberOfLegs - b.numberOfLegs;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return animals;
 }
 
 export {
