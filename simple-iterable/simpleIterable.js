0732762662609e93c1424bed70e2dc8c09acc461
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-15 23:53:00 +0530

    solve question simple iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,25 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  return {
+    [Symbol.iterator]() {
+      let count = 0;
+      return {
+        next() {
+          count += 1;
+          if (count <= 5) {
+            return {
+              value: count,
+              done: false,
+            };
+          }
+          return {
+            value: undefined,
+            done: true,
+          };
+        },
+      };
+    },
+  };
 }
 
 export {
