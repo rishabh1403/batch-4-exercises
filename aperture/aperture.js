81f33881961238ff0c087240585d7a2e1f3f2b26
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-16 04:24:25 +0530

    complete aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,15 @@
-
-function aperture(...args) {
-  return args;
+// TODO refactor this
+function aperture(splitSize, array) {
+  if (splitSize > array.length) {
+    return [];
+  }
+  const splitArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (splitSize + i <= array.length) {
+      splitArray.push(array.slice(i, splitSize + i));
+    }
+  }
+  return splitArray;
 }
 
 export {
