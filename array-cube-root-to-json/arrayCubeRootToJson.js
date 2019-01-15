a563c45eee70c7ba5c2366b8054038dc45e3ef64
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-16 04:44:54 +0530

    complete array cube root

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,22 @@
+function isNumber(num) {
+  return typeof num === 'number';
+}
+function not(cond) {
+  return !cond;
+}
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(listOfCubes) {
+  const isEveryElementNumber = listOfCubes.every(isNumber);
+  if (not(isEveryElementNumber)) {
+    throw new Error();
+  }
+  return listOfCubes.reduce((acc, el) => {
+    const key = el.toString();
+    return {
+      ...acc,
+      [key]: Math.cbrt(el),
+    };
+  }, {});
 }
 
 export {
