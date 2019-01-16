306e9313b9765b59f6e7bb2a972b832bfa6c7e40
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-17 00:24:21 +0530

    onchange in progress

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,23 @@
 
-function onChange(...args) {
-  return args;
+function onChange(detectChange, cb) {
+  // if (not(Array.isArray(array))) {
+  //   throw new TypeError(/Only arrays are supported/);
+  // }
+  const proxy = new Proxy(detectChange, {
+    set() {
+      cb();
+      return true;
+    },
+    defineProperty() {
+      cb();
+      return true;
+    },
+    deleteProperty() {
+      cb();
+      return true;
+    },
+  });
+  return proxy;
 }
 
 export {
