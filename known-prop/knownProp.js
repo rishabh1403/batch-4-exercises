8844edd64a2bb9a256a386ee644afe21dad75a86
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-17 00:09:27 +0530

    complete exercise know prop

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,14 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(prop) {
+  const proxy = new Proxy(prop, {
+    get(target, key) {
+      if (Object.prototype.hasOwnProperty.call(target, key)) {
+        return target[key];
+      }
+      throw new TypeError(/Only arrays are supported/);
+    },
+  });
+  return proxy;
 }
 
 export {
--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,32 @@
+function negate(num) {
+  return -num;
+}
+function not(cond) {
+  return !cond;
+}
+function calculateKey(key, array) {
+  if (key < 0) {
+    return array.length - negate(key);
+  }
+  return key;
+}
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  if (not(Array.isArray(array))) {
+    throw new TypeError(/Only arrays are supported/);
+  }
+  const newArray = array;
+  const proxy = new Proxy(newArray, {
+    get(target, key) {
+      const newKey = calculateKey(key, target);
+      return target[newKey];
+    },
+    set(target, key, value) {
+      const newKey = calculateKey(key, target);
+      return Reflect.set(target, newKey, value);
+    },
+  });
+  return proxy;
 }
 
 export {
