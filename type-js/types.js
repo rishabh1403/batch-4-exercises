58e47e4d3a2708bfa0cf53e087ae44bca30002d2
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-17 04:43:55 +0530

    complete type

--- type-js/types.js
+++ type-js/types.js
@@ -1,6 +1,24 @@
 
-function type(...args) {
-  return args;
+function type(value) {
+  if (Array.isArray(value)) {
+    return 'Array';
+  }
+  if (typeof value === 'number') {
+    return 'Number';
+  }
+  if (typeof value === 'string') {
+    return 'String';
+  }
+  if (value instanceof RegExp) {
+    return 'RegExp';
+  }
+  if (value === null) {
+    return 'Null';
+  }
+  if (value === undefined) {
+    return 'Undefined';
+  }
+  return 'Object';
 }
 
 export {
