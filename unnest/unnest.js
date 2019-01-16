d221fd729d279c80d3b8683a4e5300bb2ef91be7
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-17 04:56:43 +0530

    complete unnest

--- unnest/unnest.js
+++ unnest/unnest.js
@@ -1,5 +1,9 @@
 
 function unnest(list) {
+  if (!Array.isArray(list)) {
+    // eslint-disable-next-line
+    list = Object.values(list).filter(el => Array.isArray(el));
+  }
   return list.reduce((acc, el) => {
     if (Array.isArray(el)) {
       return [...acc, ...el];
