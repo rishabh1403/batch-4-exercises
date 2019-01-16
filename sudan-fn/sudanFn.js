151c4c54d185b68a37873ffb8a36a46ebd94d4ef
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-17 04:39:02 +0530

    complete tiles

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,21 @@
 
-function sudanFn(...args) {
-  return args;
+function sundanFn({ n, x, y }) {
+  if (n === 0) {
+    return x + y;
+  }
+  if (y === 0 && n >= 0) {
+    return x;
+  }
+  if (y > 0 && n >= 0) {
+    return sundanFn({
+      n: n - 1,
+      x: sundanFn({ n, x, y: y - 1 }),
+      y: sundanFn({ n, x, y: y - 1 }) + y,
+    });
+  }
+  return true;
 }
 
 export {
-  sudanFn,
+  sundanFn,
 };
--- tiles/tiles.js
+++ tiles/tiles.js
@@ -1,6 +1,17 @@
 
-function tiles(...args) {
-  return args;
+function tiles({ small, big, target }) {
+  const bigBlocks = target / 5;
+  let smallBlocks = target % 5;
+  if (bigBlocks <= big && smallBlocks <= small) {
+    return true;
+  }
+  if (bigBlocks > big) {
+    smallBlocks += ((bigBlocks - big) * 5);
+    if (smallBlocks <= small) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export {
