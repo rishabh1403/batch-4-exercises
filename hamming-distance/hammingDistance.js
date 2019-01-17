780c8cd68408f213b415f9d22c033a71decc42ee
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-18 00:17:08 +0530

    complete hamming distance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,14 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(sequence1, sequence2) {
+  if (sequence1.length !== sequence2.length) {
+    throw new Error();
+  }
+  return Array.from(sequence1).reduce((acc, element, index) => {
+    if (sequence1[index] === sequence2[index]) {
+      return acc;
+    }
+    return acc + 1;
+  }, 0);
 }
 
 export {
