ca1c726f61785de4c42588285651dd60513fb992
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-18 00:30:48 +0530

    complete prime test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,15 @@
-
-function primalityTest(...args) {
-  return args;
+function generateRangeArray(numberToCheckForPrime) {
+  return new Array(numberToCheckForPrime)
+    .fill()
+    .map((el, index) => index)
+    .filter(el => el >= 2 && el < numberToCheckForPrime);
+}
+function primalityTest(numberToCheckForPrime) {
+  if (numberToCheckForPrime < 2) {
+    return false;
+  }
+  const numberRange = generateRangeArray(numberToCheckForPrime);
+  return numberRange.every(currentNumber => numberToCheckForPrime % currentNumber !== 0);
 }
 
 export {
