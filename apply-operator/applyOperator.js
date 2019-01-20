b31baecfbe4132f2c99cc15a70f94b8514c08a0f
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-21 03:58:19 +0530

    add apply operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,19 @@
-
+// eslint-disable
 function applyOperator(...args) {
-  return args;
+  if (args.length === 0) {
+    throw new Error();
+  }
+  const [operator, ...numbers] = args;
+  if (operator === '+' || operator === '-') {
+    return numbers.reduce((acc, el) => {
+      return eval(`${acc}${operator}${el}`);
+    }, 0);
+  } else if (operator === '/' || operator === '*') {
+    return numbers.reduce((acc, el) => {
+      return eval(`${acc}${operator}${el}`);
+    }, 1);
+  }
+  return 0;
 }
 
 export {
