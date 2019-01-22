0a9d9934979d3dccf6c774f366ee93918b5857fb
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-23 04:55:11 +0530

    add two new component implementation

--- intermediate-exercises/src/components/02-StopWatch.js
+++ intermediate-exercises/src/components/02-StopWatch.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
 
-/*
-* Exercise 2:
-*
-*  Create a `StopWatch` component that has a Start/Stop button and a Clear
-*  button.
-*
-*  Pressing Start will start a timer and the lapsed time in
-*  milliseconds should be displayed above the buttons.
-*
-*  Once started the Start button should change to Stop. Clicking Stop
-*  will stop the timer but lapsed time will be preserved.
-*
-*  Clicking Start again will resume the timer from where it left off.
-*
-*  Clicking Clear will stop the timer if it's running and reset the lapsed time to 0.
-*/
-
+let timer = null;
 class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 0,
+      start: true
+    };
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+  }
+
+  startTimer() {
+    timer = setInterval(() => {
+      this.setState({
+        timer: this.state.timer + 1,
+      });
+    }, 1);
+    this.setState({
+      start: false,
+    });
+  }
+
+  stopTimer() {
+    clearInterval(timer);
+    this.setState({
+      start: true,
+    });
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.setState({
+      timer: 0,
+    });
+  }
+
+  componentWillUnmount() {
+    this.resetTimer();
+  }
+
   render() {
     return (
-      <div>Stop Watch</div>
+      <div>
+        <p>Stop Watch</p>
+        <p>{this.state.timer}</p>
+        {this.state.start ?
+          <button onClick={this.startTimer}>Start</button> :
+          <button onClick={this.stopTimer}>Stop</button>
+        }
+        <button onClick={this.resetTimer}>Reset</button>
+      </div>
     );
   }
 }
