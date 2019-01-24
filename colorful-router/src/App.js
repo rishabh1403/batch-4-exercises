66b20e17a5d325af605cc8a05ead2cca4c2628d9
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-25 04:52:25 +0530

    add question colorful router

--- colorful-router/src/App.js
+++ colorful-router/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-// import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import './styles/App.css';
 import ColorList from './components/ColorList';
-// import Color from './components/Color';
-// import NewColor from './components/NewColor';
+import Color from './components/Color';
+import NewColor from './components/NewColor';
+
+const getSelectedColor = (state) => {
+  return (props) => {
+    const data = state.find(el => el.name === props.match.params.name);
+    return <Color color={data} />;
+  };
+};
 
 class App extends Component {
   constructor(props) {
@@ -33,12 +40,17 @@ class App extends Component {
   }
 
   render() {
-    const colorListComponent = () => (
-      <ColorList colors={this.state.colors} />
-    );
-
     return (
-      colorListComponent()
+      <BrowserRouter>
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/colors" render={props => <ColorList {...props} colors={this.state.colors} />} />
+            <Route exact path="/colors/new" render={props => <NewColor {...props} addColor={this.handleAdd} colors={this.state.colors} />} />
+            <Route exact path="/colors/:name" component={getSelectedColor(this.state.colors)} />
+            <Route exact path="/" render={() => <Redirect to="/colors" />} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
--- colorful-router/src/components/ColorList.js
+++ colorful-router/src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import '../styles/ColorList.css';
@@ -6,14 +7,14 @@ import '../styles/ColorList.css';
 const ColorList = (props) => {
   const colorLinks = props.colors.map(color => (
     <li key={color.hex}>
-      {color.name}
+      <Link to={`/colors/${color.name}`}>{color.name}</Link>
     </li>
   ));
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to the Colorful Router.</h1>
-        <h1>Add a color</h1>
+        <h1><Link to="/colors/new">Add a color</Link></h1>
       </header>
       <div className="App-intro">
         <p>Please select a color.</p>
