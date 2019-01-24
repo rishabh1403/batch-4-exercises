31ce465420185773b8a79742e63ce1cffb75471f
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-25 01:48:01 +0530

    in progress

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(fn, callCount) {
+  let count = 0;
+  return (...args) => {
+    if (count < callCount) {
+      count += 1;
+      return fn(...args);
+    }
+    return null;
+  };
 }
 
 export {
--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,22 +1,25 @@
-function isPrime(i) {
-  var isPrime = true;
+const range = n => new Array(n).fill().map((el, index) => index + 1);
+const isPrime = n => range(n).every(i => )
 
-  for (var j = 2; j < i; j++) {
-    if (i % j === 0) {
-      isPrime = false;
+  // function isPrime(i) {
+  //   var isPrime = true;
+
+  //   for (var j = 2; j < i; j++) {
+  //     if (i % j === 0) {
+  //       isPrime = false;
+  //     }
+  //   }
+  //   return isPrime;
+  // }
+  function sumPrimes(n) {
+    let sum = 0;
+    for (let i = 2; i <= n; i++) {
+      if (isPrime(i)) {
+        sum += i;
+      }
     }
+    return sum;
   }
-  return isPrime;
-}
-function sumPrimes(n) {
-  let sum = 0;
-  for(let i=2;i<=n;i++){
-    if(isPrime(i)){
-      sum += i;
-    }
-  }
-  return sum;
-}
 
 export {
   sumPrimes,
