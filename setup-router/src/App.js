5b7f96b4cab66256303ded3e90c9faaf73f4e5d6
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-25 00:15:27 +0530

    add question setup router

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component, Fragment } from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeName: 'home',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(name) {
+    this.setState({
+      activeName: name,
+    });
+  }
+
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+        <li className={this.state.activeName === 'home' ? 'active' : ''}>
+          <Link onClick={() => this.handleClick('home')} to="/">Home</Link>
+        </li>
+        <li className={this.state.activeName === 'about' ? 'active' : ''}>
+          <Link onClick={() => this.handleClick('about')} to="/about">About Us</Link>
+        </li>
+        <li className={this.state.activeName === 'contact' ? 'active' : ''}>
+          <Link onClick={() => this.handleClick('contact')} to="/contact">Contact</Link >
+        </li>
+      </ul >
     );
   }
 }
