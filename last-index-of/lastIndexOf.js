94ff332bf57fdbc75a1ffc10db38bc1b43f8b8bb
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-29 00:24:31 +0530

    add question lastIndexOf

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,11 @@
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(element, list) {
+  return list.reduce((acc, el, index) => {
+    if (el === element) {
+      return index;
+    }
+    return acc;
+  }, -1);
 }
 
 export {
