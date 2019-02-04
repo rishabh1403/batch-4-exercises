34d61d6fa9c3e71e4bcf9c8e18fd0393164e4ebb
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-05 04:52:42 +0530

    add question strip path

--- strip-path/stripPath.js
+++ strip-path/stripPath.js
@@ -1,6 +1,13 @@
+const sanitizePath = (path) => {
+  const pathArray = path.split('/');
+  return pathArray.filter((el, index) => el !== '..' && pathArray[index + 1] !== '..').join('/');
+};
 
-function stripPath(...args) {
-  return args;
+function stripPath(path, pathToStrip) {
+  if (!pathToStrip) {
+    return path;
+  }
+  return sanitizePath(path).replace(`${sanitizePath(pathToStrip)}/`, '');
 }
 
 export {
