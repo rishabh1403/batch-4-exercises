564232b5be0627b256a82f313a082d9cacac2549
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-05 00:46:44 +0530

    incomplete question stripeof

--- strip-eof/stripEof.js
+++ strip-eof/stripEof.js
@@ -1,6 +1,18 @@
 
-function stripEof(...args) {
-  return args;
+const stripEndOfFile = (text) => {
+  if (text.endsWith('\r\n')) {
+    text.replace('\r\n', '');
+  }
+  text.replace('\n', '');
+  return text;
+};
+
+function stripEof(text) {
+  if (Buffer.isBuffer(text)) {
+    // eslint-disable-next-line no-param-reassign
+    text = text.toString('utf-8');
+  }
+  return stripEndOfFile(text);
 }
 
 export {
