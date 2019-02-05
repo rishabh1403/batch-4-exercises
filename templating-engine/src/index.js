8c00aae9c54313b0e9c5590e2ab38a7bad20e7d9
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-05 23:44:55 +0530

    add question templating engines

--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const languages = ['JavaScript', 'Haskell', 'Python'];
+app.set('view engine', 'ejs');
+
+const languages = ['JavaScript', 'Haskell', 'Python', 'Go Lang', 'Dart'];
 
 app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
+  res.render('../views/index', {
+    languages,
+  });
 });
 
 app.listen(3000, () => {
