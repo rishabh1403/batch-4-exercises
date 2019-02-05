cad754dcbc9c6eb22c124522753230386b3dc595
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-06 00:33:39 +0530

    add question express router [incomplete]

--- express-router/router/index.js
+++ express-router/router/index.js
@@ -1,5 +1,73 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
+
+const readFileContents = () => new Promise((resolve, reject) => {
+  fs.readFile('data', 'utf-8', (err, data) => {
+    if (err) {
+      reject(err);
+    }
+    const languages = JSON.parse(data).languages;
+    resolve(languages);
+  });
+});
+
+const writeContentToFile = data => new Promise((resolve, reject) => {
+  fs.writeFile('data', JSON.stringify(data), (err, data) => {
+    if (err) {
+      reject(err);
+    }
+    resolve();
+  });
+});
+
+const getItemById = (items, id) => items.filter(item => item["id"] === parseInt(id))[0];
+
+
+router.get('/', (req, res) => {
+  readFileContents()
+    .then((languages) => {
+      res.render('../views/index', {
+        languages,
+      });
+    }).catch((err) => {
+      res.sendStatus(500);
+    });
+})
+
+router.post('/', (req, res) => {
+  readFileContents()
+    .then((languages) => {
+      languages.push({ "name": req.body.name, "id": languages.length });
+      const data = {
+        languages,
+      }
+      return writeContentToFile(data);
+    }).then(() => {
+      res.redirect('/')
+    }).catch((err) => {
+      res.sendStatus(500);
+    });
+})
+
+router.get('/new', (req, res) => {
+  res.render('../views/new');
+})
+
+router.get('/:id/edit', (req, res) => {
+  readFileContents()
+    .then((languages) => {
+      return getItemById(languages, req.params.id);
+    }).then((language) => {
+      res.render('../views/edit', {
+        language,
+      });
+    }).catch((err) => {
+      res.sendStatus(500);
+    });
+})
+
 
 module.exports = router;
