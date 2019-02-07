51cea8f7d7666506d15184d80b9ea282ae814e1d
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-08 00:37:44 +0530

    add question titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,18 +1,24 @@
 const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const not = cond => !cond;
+
+const capitalizeFirstCharOfWord = (sentence, character, index) => {
+  if (index > 0) {
+    if (not(letters.includes(sentence[index - 1]))) {
+      return character.toUpperCase();
+    }
+    return character;
+  }
+  if (letters.includes(sentence[index])) {
+    return character.toUpperCase();
+  }
+  return character;
+};
 
 function titleize(sentence) {
   let transformedSentence = sentence.toLowerCase().split('');
-  transformedSentence = transformedSentence.map((el, index) => {
-    if (index > 0) {
-      if (!letters.includes(transformedSentence[index - 1])) {
-        return el.toUpperCase();
-      }
-      return el;
-    }
-    if (letters.includes(transformedSentence[index])) {
-      return el.toUpperCase();
-    }
-    return el;
+  // eslint-disable-next-line max-len
+  transformedSentence = transformedSentence.map((el, index) => { // eslint-disable-line arrow-body-style
+    return capitalizeFirstCharOfWord(transformedSentence, el, index);
   });
   return transformedSentence.join('');
 }
