441da3a939c1a2f755910a3d52cc608cebb77031
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-08 00:17:30 +0530

    add question random

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,6 +1,14 @@
 
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(start, end) {
+  let randomValue = start;
+  let newRandom = start;
+  return () => {
+    while (newRandom === randomValue) {
+      newRandom = Math.floor(Math.random() * end) + start;
+    }
+    randomValue = newRandom;
+    return randomValue;
+  };
 }
 
 export {
