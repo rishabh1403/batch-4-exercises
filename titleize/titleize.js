c59c5e2a5cc8b752554a4f567914dc4380571e0e
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-08 00:04:43 +0530

    add question titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,20 @@
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-function titleize(...args) {
-  return args;
+function titleize(sentence) {
+  let transformedSentence = sentence.toLowerCase().split('');
+  transformedSentence = transformedSentence.map((el, index) => {
+    if (index > 0) {
+      if (!letters.includes(transformedSentence[index - 1])) {
+        return el.toUpperCase();
+      }
+      return el;
+    }
+    if (letters.includes(transformedSentence[index])) {
+      return el.toUpperCase();
+    }
+    return el;
+  });
+  return transformedSentence.join('');
 }
 
 export {
